6c591c11364e7fe49f9789ec33440417db2bb34d
May Thu <maythuzarlinn96@gmail.com>
2022-02-21 20:00:21 +0630

    feature/tutorial_02 Age Calculator.

--- tutorials/tutorial2/tutorial_2.rb
+++ tutorials/tutorial2/tutorial_2.rb
@@ -7,7 +7,6 @@ birth_year = gets.chomp.to_i
 current_year = Time.now.year.to_i
 # age-calculation
 age = current_year - 1 - birth_year
-# print age
-puts("You are #{age}.")
+puts("\n")
 # checking age : adult or child and printing result
-age > 18 ? puts("#{age}, adult") : puts("#{age}, child")
+age > 18 ? puts("You are #{age}, adult") : puts("#{age}, child")
