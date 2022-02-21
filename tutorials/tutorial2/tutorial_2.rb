ea0cfe33b4961f8c74311db13cd993aece6ca2bb
May Thu <maythuzarlinn96@gmail.com>
2022-02-21 20:21:42 +0630

    feature/tutorial_02 Age Caculation.

--- tutorials/tutorial2/tutorial_2.rb
+++ tutorials/tutorial2/tutorial_2.rb
@@ -7,6 +7,5 @@ birth_year = gets.chomp.to_i
 current_year = Time.now.year.to_i
 # age-calculation
 age = current_year - 1 - birth_year
-puts("\n")
 # checking age : adult or child and printing result
 age > 18 ? puts("You are #{age}, adult") : puts("#{age}, child")
