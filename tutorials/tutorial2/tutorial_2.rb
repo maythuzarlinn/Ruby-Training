e43ccea306a8247c7ffc482f138e97361c36d350
May Thu <maythuzarlinn96@gmail.com>
2022-02-21 19:41:10 +0630

    modified:   tutorials/tutorial2/tutorial_2.rb

--- tutorials/tutorial2/tutorial_2.rb
+++ tutorials/tutorial2/tutorial_2.rb
@@ -2,11 +2,11 @@ require "date"
 # ask user input with command prompt
 puts("Please let us know your date of birth.Enter your DOB:")
 # get user's age
-birthYear = gets.chomp.to_i
+birth_year = gets.chomp.to_i
 # current year
-currentYear = Time.now.year.to_i
-# age calculation
-age = currentYear - 1 - birthYear
+current_year = Time.now.year.to_i
+# age-calculation
+age = current_year - 1 - birth_year
 # print age
 puts("You are #{age}.")
 # checking age : adult or child and printing result
