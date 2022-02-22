b713899ea29394fa9e78a43c7ed37c35edcd0e39
May Thu <maythuzarlinn96@gmail.com>
2022-02-22 17:56:16 +0630

    feature/tutorial_05 Updated according to review.

--- tutorials/tutorial5/tutorial_5.rb
+++ tutorials/tutorial5/tutorial_5.rb
@@ -2,24 +2,10 @@ require "date"
 current_day = Date.today
 day_name = current_day.strftime("%A")
 
-last_1_day = current_day - 1
-last_1_day_name = last_1_day.strftime("%A")
-
-last_2_day = current_day - 2
-last_2_day_name = last_2_day.strftime("%A")
-
-last_3_day = current_day - 3
-last_3_day_name = last_3_day.strftime("%A")
-
-last_4_day = current_day - 4
-last_4_day_name = last_4_day.strftime("%A")
-
-last_5_day = current_day - 5
-last_5_day_name = last_5_day.strftime("%A")
-
-puts "Today is        #{current_day} ( #{day_name} )"
-puts "1.Yesterday is  #{last_1_day} ( #{last_1_day_name} )"
-puts "2.Last 2 day is #{last_2_day} ( #{last_2_day_name} )"
-puts "3.Last 3 day is #{last_3_day} ( #{last_3_day_name} )"
-puts "4.Last 4 day is #{last_4_day} ( #{last_4_day_name} )"
-puts "5.Last 5 day is #{last_5_day} ( #{last_5_day_name} )"
+6.times do |i|
+  last_days = current_day - i
+  last_days_name = last_days.strftime("%A")
+  puts "Today is #{last_days} ( #{last_days_name} )" if i == 0
+  i == 1 ? (puts "1. Yesterday is #{last_days} ( #{last_days_name} )") :
+    (puts "#{i}. Last #{i} day is #{last_days} ( #{last_days_name} )") unless i == 0
+end
