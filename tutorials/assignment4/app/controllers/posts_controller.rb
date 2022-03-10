16132b019f5c52c26d5a20abbe506dcaa0b4a70f
May Thu <maythuzarlinn96@gmail.com>
2022-03-11 02:56:38 +0630

    Added Assignment 4 Working with Ajax on ROR

---
+++ tutorials/assignment4/app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+    @post = UserService.getUserByID(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @is_post_create = PostService.createPost(@post)
+    if @is_post_create
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+        format.js
+      end
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = PostService.getPostByID(params[:id])
+  end
+
+  def update
+    @post = PostService.getPostByID(params[:id])
+    @is_post_update = PostService.updatePost(@post, post_params)
+    if @is_post_update
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = PostService.getPostByID(params[:id])
+    PostService.destroy(@post)
+
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.json { head :no_content }
+      format.js { render :layout => false }
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :status)
+  end
+end
